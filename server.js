00a9bea013c8ad93dea7ce97ffc816d0db217a35
Mizuno <se.mizuno.1122@gmail.com>
2014-10-22 01:56:29 +0900

    deferredを用いた実装に修正 (動作確認済み)

--- server.js
+++ server.js
@@ -4,12 +4,25 @@ var http = require('http');
 var url = require('url');
 var nutrition = require('NutritionMongoHelper.js');
 
+
 //サーバの作成
 var server = http.createServer();
 console.log("Server Create - Success");
 
+
+//イベントハンドラを登録
+server.on('request', requestHandler);
+
+//サーバを起動
+var port = 8080;
+server.listen(port, function() {
+	console.log("Server Listen Start - Success");
+});
+
+
+
 //リクエスト受信時のハンドラ
-var requestHandler = function(request, response) {
+function requestHandler(request, response) {
 
 	//GETメソッド以外は受け付けない
 	if (request.method != 'GET') {
@@ -22,43 +35,49 @@ var requestHandler = function(request, response) {
 
 	//ここでアクセスしてきたパスによって処理を分岐
 	var result = null;
+	
 	switch (path) {
 		
 		case '/nutrition':
-		    if (queries['names'] != undefined) {
+		    
+		    if (queries['names']) {
+		        
 		        var foodNames = queries['names'];
-				result = nutrition.getNutritionsByFoodNames(foodNames);
+				nutrition.getNutritionByFoodNames(foodNames)
+
+				.done( function(result) {
+					respondResult(response, result);
+				},
+
+				function(err) {
+					console.log(err);
+				});
+
 			}			
 			break;
 
 		default:
+			respondResult(response, result);
 			break;	
 	}
+};
 
+
+//結果をresponseに記述
+function respondResult(response, result) {
 	//レスポンスヘッダを記述
 	var statusCode = 200;
+	
 	var headers = {
 		'Content-Type': 'application/json charset=UTF-8'
 	}
 	response.writeHead(statusCode, headers);
 
-
 	//レスポンスボディを記述
 	var encode = 'UTF-8';
 	response.write('nutrition:' + result + '\n', encode);
-	
+					
 	//レスポンスの終了
 	response.end();
-};
-
-//イベントハンドラを登録
-server.on('request', requestHandler);
-
-//サーバを起動
-//引数は(port, [hostname], [backlog], [callback])
-var port = 8080;
-server.listen(port, function() {
-	console.log("Server Listen Start - Success");
-});
-
+}
 
